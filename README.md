971834c5da8139dbc1cfab039e591f3726ddfda7
demdecuong <nguyenphucminh2804@gmail.com>
2021-07-09 20:57:23 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Installation
+`pip install requirement.txt`
+
+### Training model 
+```
+python main.py 
+```
+Please go to `config.py` for custom config
---
+++ model/layer/__init__.py
@@ -0,0 +1,5 @@
+from model.layer.charcnn import *
+from model.layer.FeedForward import *
+from model.layer.featureEmbed import *
+from model.layer.wordnet import *
+from model.layer.biaffine import *
