cf42f3752553c1ef0b9322608ce6251d9591d21e
demdecuong <nguyenphucminh2804@gmail.com>
2021-07-16 15:09:49 +0700

    add synthesize data

--- data/format_testset.py
+++ data/format_testset.py
@@ -1,15 +1,16 @@
 '''
     Convert csv testset format into trainable format
 '''
-
+import os
 import re
+import random
 import pandas as pd
 import json
 from transformers import AutoTokenizer
 from vncorenlp import VnCoreNLP
 
 # tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
-tokenizer = VnCoreNLP("../vncorenlp/VnCoreNLP-1.1.1.jar", annotators="wseg", max_heap_size='-Xmx500m')
+# tokenizer = VnCoreNLP("../vncorenlp/VnCoreNLP-1.1.1.jar", annotators="wseg", max_heap_size='-Xmx500m')
 
 def padding_punct(s):
     s = re.sub('([.,!?()])', r' \1 ', s)
@@ -37,9 +38,8 @@ def find_sub_list(sl,l):
             results.append((ind,ind+sll-1))
     return results
 
-if __name__ == '__main__':
-    # missing name is nan
-    testset = pd.read_csv("person_name/human_testset.csv", encoding='utf-8')
+def format_csv(path="person_name/human_testset.csv",out_path="person_name/human_test.json"):
+    testset = pd.read_csv(path, encoding='utf-8')
     testset['name_1'].fillna("nan")
     testset['name_2'].fillna("nan")
     trg = testset['input'].tolist()
@@ -87,4 +87,72 @@ if __name__ == '__main__':
                 tmp.append([start_idx,end_idx,"PERSON"])
         label.append(tmp)
 
-    save_to_json(trg,label,"person_name/human_test.json")
+    save_to_json(trg,label,out_path)
+
+def read_txt(path):
+    data = []
+    with open(path,'r') as f:
+        for line in f:
+            data.append(line.replace('\n',''))
+    return data
+
+
+def from_sequence_labeling_to_json(src,label):
+    json_label = []
+    for i in range(len(src)):
+        start_idx = -1
+        end_idx = -1
+        tmp = []
+        label_seq = label[i].split(' ')
+        for idx, token in enumerate(src[i].split(' ')):
+            if label_seq[idx] == 'B-PERSON':
+                if start_idx != -1:
+                    tmp.append([start_idx,end_idx,'PERSON'])
+                start_idx = idx
+                end_idx = idx
+            elif label_seq[idx] == 'I-PERSON':
+                end_idx += 1
+            else:
+                if start_idx != -1:
+                    tmp.append([start_idx,end_idx,'PERSON'])
+                    start_idx = -1
+                    end_idx = -1
+        json_label.append(tmp)
+    
+    return src, json_label
+
+def format_aug_data(root_dir,out_path):
+    list_dir = os.listdir(root_dir)
+    for folder in list_dir:
+        src = read_txt('/'.join([root_dir,folder,'data.in']))
+        label = read_txt('/'.join([root_dir,folder,'label.out']))
+        c = list(zip(src, label))
+
+        random.shuffle(c)
+
+        src, label = zip(*c)
+
+        src = src[:5000]
+        label = label[:5000]
+        
+        src,label = from_sequence_labeling_to_json(src,label)
+
+        print(len(src),len(label))
+
+    save_to_json(src,label,out_path)
+
+if __name__ == '__main__':
+    # From human_test.csv to human_test.json
+    # format_csv()
+
+    format_aug_data('aug_data','person_name/train_synth.json')
+    # src =[ 
+    #     'hoa phương là tên ông đặt buổi hẹn nay da vàng',
+    #     'chị book lịch dùm buổi gặp tại bệnh viện trung ươn VinMec vào THÁNG 4 27, 1985 thì làm sao'
+    # ]
+    # label = [
+    #     'B-PERSON I-PERSON O O O O O O O O O',
+    #     'O O O O O O O O O O O O O B-DATE I-DATE I-DATE I-DATE O O O'
+    # ]
+    # a,b = from_sequence_labeling_to_json(src,label)
+    # print(a,b)
